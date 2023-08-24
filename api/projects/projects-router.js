f6fb4d585a833ecb4ea8dea2aac3a791c83ab2e9
Darley Peralta <ordeal00s@gmail.com>
2023-08-24 18:38:47 -0400

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,12 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const router = express.Router();
+
+
+
+router.get('/' , (req,res,next) => { //eslint-disable-line
+  res.json({message: 'aay'})
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
+
 const express = require('express');
 const server = express();
+const projectRouter = require('./projects/projects-router')
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+
+server.use(express.json())
+server.use('/api/projects', projectRouter)
+
+
 
 module.exports = server;
